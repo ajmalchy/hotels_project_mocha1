538051bb3b87d1a42b21b43f6ddaa7e4518793a8
ajmalchy <ajmal.chy.biz@gmail.com>
2024-09-09 23:25:46 -0400

    verify child age dropdowns are same as number of children selected done excep only one function

--- Pages/landingPage.js
+++ Pages/landingPage.js
@@ -3,6 +3,25 @@ class Landingpage {
     hotelsTrademarkLocator = '//a[@data-testid="header-brand-logo-anchor"]';
     signinLinkLocator = '//button[text()="Sign in"]';
     signinBtnLocator = '//a[contains(text(), "Sign in")]';
+
+    travelersBtnLocator = '//button[@data-stid="open-room-picker"]';
+
+    childrenBtnPlusLocator = '//input[@id="traveler_selector_children_step_input-0"]/following-sibling::button';
+    
+    childrenBtnMinusLocator = '//input[@id="traveler_selector_children_step_input-0"]/preceding-sibling::button';
+
+    allChildDropdownLocators = '//select[@class="uitk-field-select"]';
+
+    dropDownFieldLocator_starts = '//select[@class="uitk-field-select"]//preceding-sibling::label[text()="';
+
+    dropDownFieldLocator_ends = '"]'
+    travelersDoneBtnLocator = '//button[@id="traveler_selector_done_button"]';
+
+    totalTravelersLocator = '//button[@data-stid="open-room-picker"]';
+
+    dropDownOptions_start = '//select[@class="uitk-field-select"]//option[text()="';
+    dropDownOptions_end = '"]';
+    childrenNumberLocator = '//input[@id="traveler_selector_children_step_input-0"]';
     // user clicks on trademark
   
     // functions to interact with the elements on landing page
@@ -21,6 +40,67 @@ class Landingpage {
         await $(this.signinBtnLocator).waitForClickable();
         await $(this.signinBtnLocator).click();
     } 
+    // User clicks on travelers button
+    async clickTravelersBtn() {
+        await $(this.travelersBtnLocator).waitForClickable();
+        await $(this.travelersBtnLocator).click();
+    }
+
+    // User selects children as 2
+    /**
+     * get children number
+     * click plus or minus btn to select desired of children
+     */
+    async getChildrenNumberValue() {
+        await $(this.childrenNumberLocator).waitForDisplayed();
+        const childrenNumberInput = await $(this.childrenNumberLocator);
+        const numberOfChildrenText = await childrenNumberInput.getAttribute("value");
+        const actualChildrenNumber = parseInt(numberOfChildrenText);
+        return actualChildrenNumber;
+    }
+    async clickChildrenBtnPlus() {
+        await $(this.childrenBtnPlusLocator).waitForClickable();
+        await $(this.childrenBtnPlusLocator).click();
+    }
+    async clickChildrenBtnMinus() {
+        await $(this.childrenBtnMinusLocator).waitForClickable();
+        await $(this.childrenBtnMinusLocator).click();
+    }
+    // user verifies that children dropdown count is 2
+    /**
+     * get visible children dropdown count
+     * verify that visible dropdown count is 2 or isDisplayed
+     * 
+     */
+
+    // problem
+    async getVisibleChildrenDropdownCount() {
+        const dropdownElements = await $$(this.allChildDropdownLocators);
+        return await dropdownElements.filter((element) => element.isDisplayed()).length;
+        
+    }
+    async isChildrenDropdownDisplayed() {
+        const dropdownElements = await $$(this.allChildDropdownLocators);
+        
+        if (dropdownElements.length > 0) {
+            const dropdownElement = dropdownElements.find(async (element) => await element.isDisplayed());
+            return dropdownElement;
+        } else {
+            return false;
+        }
+    }
+
+    async isButtonEnabled(buttonType) {
+        let buttonSelector;
+        if (buttonType == 'Plus') {
+            buttonSelector = '//input[@id="traveler_selector_children_step_input-0"]/following-sibling::button';
+        } else if (buttonType == 'Minus') {
+            buttonSelector = '//input[@id="traveler_selector_children_step_input-0"]/preceding-sibling::button';
+        }
+        const buttonElement = await $(buttonSelector);
+        return await buttonElement.isEnabled();
+
+    }
 
 }
 
--- tests/session1/sprint1Of1.js
+++ tests/session1/sprint1Of1.js
@@ -4,18 +4,18 @@ const signinPage = require("../../Pages/signinPage");
 
 // refer test suite
 describe('Sprint 1', () => {
-    it('User goes to hotels website', async () => {
+    it.only('User goes to hotels website', async () => {
         // go to hotels.com
         await browser.url('https://www.hotels.com/')
         await browser.pause(5000);
         await landingPage.clickTradeMarkToRefresh();
         await browser.pause(4000);
     })
-    
+
     it('User clicks on signin link', async () => {
         await landingPage.clickSigninLink();
         await browser.pause(2000);
-    } )
+    })
 
     it('User clicks signin btn', async () => {
         await landingPage.clickSigninBtn();
@@ -45,8 +45,198 @@ describe('Sprint 1', () => {
     it('User verifies error message is displayed', async () => {
         await signinPage.invalidEmailErrorIsDisplayed();
     })
-   
-
+// Verify Child-age dropdowns are same as number of Children selected
+    it.only('User clicks on the travelers btn', async () => {
+        await landingPage.clickTravelersBtn();
+        await browser.pause(5000);
     })
+
+    it.only('User selects children as 2', async () => {
+        let previousChildrenCount;
+        previousChildrenCount = await landingPage.getChildrenNumberValue();
+        if (previousChildrenCount == 0) {
+            for (let i = 0; i < 2; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else if
+            (previousChildrenCount < 2) {
+            const toIncrease = 2 - previousChildrenCount;
+            for (let i = 0; i < toIncrease; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else {
+            const toDecrease = previousChildrenCount - 2;
+            for (let i = 0; i < toDecrease; i++) {
+                await landingPage.clickChildrenBtnMinus();
+                await browser.pause(2000);
+            }
+        }
+    })
+// problem
+    it('User verifies that Children-age dropwdowns are 2', async () => {
+        const actualDropDownCount = await landingPage.getVisibleChildrenDropdownCount();
+        expect(actualDropDownCount).to.equal(2)
+    })
+    it.only('User verifies that the plus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Plus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+    it.only('User verifies that the minus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Minus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+
+    it.only('User selects children as 6', async () => {
+        let previousChildrenCount;
+        previousChildrenCount = await landingPage.getChildrenNumberValue();
+        if (previousChildrenCount == 0) {
+            for (let i = 0; i < 6; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else if
+            (previousChildrenCount < 6) {
+            const toIncrease = 6 - previousChildrenCount;
+            for (let i = 0; i < toIncrease; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else {
+            const toDecrease = previousChildrenCount - 6;
+            for (let i = 0; i < toDecrease; i++) {
+                await landingPage.clickChildrenBtnMinus();
+                await browser.pause(2000);
+            }
+        }
+    })
+
+    it.only('User verifies that the plus btn is disabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Plus");
+        const expectedState = "disabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+
+    it.only('User verifies that the minus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Minus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+
+    it.only('User selects children as 5', async () => {
+        let previousChildrenCount;
+        previousChildrenCount = await landingPage.getChildrenNumberValue();
+        if (previousChildrenCount == 0) {
+            for (let i = 0; i < 5; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else if
+            (previousChildrenCount < 5) {
+            const toIncrease = 6 - previousChildrenCount;
+            for (let i = 0; i < toIncrease; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else {
+            const toDecrease = previousChildrenCount - 5;
+            for (let i = 0; i < toDecrease; i++) {
+                await landingPage.clickChildrenBtnMinus();
+                await browser.pause(2000);
+            }
+        }
+    })
+    it.only('User verifies that the plus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Plus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+    it.only('User verifies that the minus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Minus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+
+    it.only('User selects children as 0', async () => {
+        let previousChildrenCount;
+        previousChildrenCount = await landingPage.getChildrenNumberValue();
+        if (previousChildrenCount == 0) {
+            for (let i = 0; i < 5; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else if
+            (previousChildrenCount < 0) {
+            const toIncrease = 0 - previousChildrenCount;
+            for (let i = 0; i < toIncrease; i++) {
+                await landingPage.clickChildrenBtnPlus();
+                await browser.pause(2000);
+            }
+        } else {
+            const toDecrease = previousChildrenCount - 0;
+            for (let i = 0; i < toDecrease; i++) {
+                await landingPage.clickChildrenBtnMinus();
+                await browser.pause(2000);
+            }
+        }
+    })
+    it.only('User verifies that Children age dropdowns are not displayed', async () => {
+        const isDropdownDisplayed = await landingPage.isChildrenDropdownDisplayed();
+        expect(await isDropdownDisplayed).to.be.false;
+    })
+    it.only('User verifies that the plus btn is enabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Plus");
+        const expectedState = "enabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+    it.only('User verifies that the minus btn is disabled', async () => {
+        const isButtonEnabled = await landingPage.isButtonEnabled("Minus");
+        const expectedState = "disabled";
+
+        if (expectedState === "enabled") {
+            expect(isButtonEnabled).to.be.true;
+        } else if (expectedState === "disabled") {
+            expect(isButtonEnabled).to.be.false;
+        }
+    })
+})
 
 
